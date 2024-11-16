566d7e9aeb3ced460b65bb60d13afe6361c9c3f2
Charity Smith <150025121+csmith709@users.noreply.github.com>
2024-11-16 12:10:04 -0330

    added docker

---
+++ src/main/java/GolfClubTournamentApplication.java
@@ -0,0 +1,9 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GolfClubTournamentApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GolfClubTournamentApplication.class, args);
+    }
+}
